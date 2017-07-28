db03cc325d21194b1255f55ac759e543dfe0868d
maxleiko <max.tricoire@gmail.com>
2017-07-28 14:31:00 +0200

    v1.1.1: allows key-ing saving

--- test/main.js
+++ test/main.js
@@ -10,6 +10,7 @@ const confFile = require('../tiny-conf-plugin-file');
 describe('main tests', function () {
 
   beforeEach(function () {
+    delete require.cache[require.resolve('./fixtures/conf0.json')];
     conf.clear();
   });
 
@@ -43,6 +44,36 @@ describe('main tests', function () {
       .then(() => {
         expect(conf.get('foo')).toEqual('bar');
         expect(conf.get('baz')).toEqual('nope');
+      });
+  });
+
+  it('should save only key data to file', function () {
+    // writing data from fixtures/conf0.json to /tmp
+    conf.set(require('./fixtures/conf0.json'));
+    const newConfPath = tmp.tmpNameSync();
+    fs.writeFileSync(newConfPath, JSON.stringify(conf.get(), null, 2), 'utf-8');
+
+    // read conf from /tmp
+    confFile(conf, newConfPath);
+
+    // be sure the cp to new-conf0.json worked
+    expect(conf.get('foo')).toEqual('bar');
+    expect(conf.get('baz.one')).toEqual(42);
+    expect(conf.get('baz.two')).toEqual(['Hello', 'World']);
+
+    // modify content
+    conf.set('baz', 'nope');
+    // modify content that should not be saved
+    conf.set('yolo', 'swag');
+
+    // save only the content of baz and merge it with filepath
+    return conf.save('baz')
+      .then(() => {
+        expect(conf.get('foo')).toEqual('bar');
+        expect(conf.get('baz')).toEqual('nope');
+        const newConfSrc = fs.readFileSync(newConfPath, 'utf-8');
+        const newConf = JSON.parse(newConfSrc);
+        expect(newConf.yolo).toEqual(undefined);
       });
   });
 });
--- tiny-conf-plugin-file.js
+++ tiny-conf-plugin-file.js
@@ -5,15 +5,29 @@ var fs = require('fs');
 module.exports = function (config, filepath) {
   config.set(JSON.parse(fs.readFileSync(filepath, 'utf8')));
 
-  config.save = function saveToFile() {
-    var data = JSON.stringify(config.get(), null, 2);
+  config.save = function saveToFile(key) {
     return new Promise(function (resolve, reject) {
-      fs.writeFile(filepath, data, 'utf-8', function (err) {
+      fs.readFile(filepath, 'utf-8', function (err, fileConf) {
         if (err) {
           reject(err);
         } else {
-          resolve();
+          resolve(JSON.parse(fileConf));
         }
+      });
+    }).then(function (fileConf) {
+      var data = fileConf;
+      if (key) {
+        data[key] = config.get(key);
+      }
+      var strData = JSON.stringify(data, null, 2);
+      return new Promise(function (resolve, reject) {
+        fs.writeFile(filepath, strData, 'utf-8', function (err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
       });
     });
   };
